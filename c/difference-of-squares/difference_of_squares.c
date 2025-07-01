136b6c39a9d3b920a16b0e5c8bbc567548f8eb1c
Juan Benjumea <benjumeamoreno@gmail.com>
2025-07-01 08:26:41 +0200

    new exercises

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,26 @@
 #include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int n)
+{
+    unsigned int sum = 0;
+    for (unsigned int i = 1; i <= n; i++)
+    {
+        sum += i * i;
+    }
+    return sum;
+}
+
+unsigned int square_of_sum(unsigned int n)
+{
+    unsigned int sum = 0;
+    for (unsigned int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum * sum;
+}
+
+unsigned int difference_of_squares(unsigned int n)
+{
+    return square_of_sum(n) - sum_of_squares(n);
+}
