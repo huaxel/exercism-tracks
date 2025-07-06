3140e8ef09a2c44a5e5fd902fb3c25766d186550
Juan Benjumea <benjumeamoreno@gmail.com>
2025-07-06 12:22:26 +0200

    collatz and resistors completed

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,17 @@
 #include "resistor_color.h"
+
+const resistor_band_t COLORS[] = {
+    BLACK, BROWN, RED, ORANGE, YELLOW,
+    GREEN, BLUE, VIOLET, GREY, WHITE
+
+};
+
+const resistor_band_t *colors(void)
+{
+    return COLORS;
+}
+
+int color_code(resistor_band_t color)
+{
+    return color;
+}
