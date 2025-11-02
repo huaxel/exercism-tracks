07c0d76a8eb0c9603097573271fa0af63fd88a36
Juan Benjumea <benjumeamoreno@gmail.com>
2025-11-02 23:26:26 +0100

    acronym

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Text.RegularExpressions;
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        var regexPattern = @"(?:^|[\s\-_`])([A-Za-z])";
+        var matches = Regex.Matches(phrase, regexPattern);
+        var acronym = string.Concat(matches.Select(m => m.Groups[1].Value)).ToUpper();
+        return acronym;
     }
 }
