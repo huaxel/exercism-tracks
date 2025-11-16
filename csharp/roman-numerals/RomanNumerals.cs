f9fc70ab063f56d6689b067577ad43b28f01725d
Juan Benjumea <benjumeamoreno@gmail.com>
2025-11-17 00:07:33 +0100

    need for speed and numerals

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,7 +1,37 @@
+using System.Collections.Generic;
+using System.Text;
 public static class RomanNumeralExtension
 {
+    private static readonly Dictionary<int, string> Numerals = new()
+    {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    };
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var remainder = value;
+        StringBuilder result = new();
+
+        foreach (var (threshold, numeral) in Numerals)
+        {
+            while (remainder >= threshold)
+            {
+                result.Append(numeral);
+                remainder -= threshold;
+            }
+        }
+
+        return result.ToString();
     }
 }
