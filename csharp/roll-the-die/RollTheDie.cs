4881f0e62ee2d4b53aa29370475b985a20fbee3c
Juan Benjumea <benjumeamoreno@gmail.com>
2025-11-16 20:42:09 +0100

    roll the die start files

---
+++ csharp/roll-the-die/RollTheDie.cs
@@ -0,0 +1,12 @@
+public class Player
+{
+    public int RollDie()
+    {
+        throw new NotImplementedException("Please implement the Player.RollDie() method");
+    }
+
+    public double GenerateSpellStrength()
+    {
+        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+    }
+}
