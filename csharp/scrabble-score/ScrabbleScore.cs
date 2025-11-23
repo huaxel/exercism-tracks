77e7348fabbbb250d3f6572e32e4a3234e3b814f
Juan Benjumea <juan.benjumea.moreno@student.ehb.be>
2025-11-23 21:53:45 +0100

    scrabble score finished

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,7 +1,22 @@
 public static class ScrabbleScore
 {
+    public static readonly Dictionary<string, int> ScoreTable = new()
+    {
+        ["AEIOULNRST"] = 1,
+        ["DG"] = 2,
+        ["BCMP"] = 3,
+        ["FHVWY"] = 4,
+        ["K"] = 5,
+        ["JX"] = 8,
+        ["QZ"] = 10
+    };
+
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrWhiteSpace(input)) return 0;
+        input = input.ToUpperInvariant();
+        return input
+        .Select(c => ScoreTable.First(kv => kv.Key.Contains(c)).Value)
+        .Sum();
     }
 }
