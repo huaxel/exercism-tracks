19de1dfd833029d8d5884a468c339d1468efe5ac
Juan Benjumea <juan.benjumea.moreno@student.ehb.be>
2025-11-23 22:46:58 +0100

    finish armstrong

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -2,6 +2,14 @@ public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var digits = (from c in number.ToString()
+                      select c - '0')
+                    .ToArray();
+
+        var power = digits.Length;
+        var sum = (from d in digits
+                   select (int)Math.Pow(d, power))
+                  .Sum();
+        return sum == number;
     }
 }
